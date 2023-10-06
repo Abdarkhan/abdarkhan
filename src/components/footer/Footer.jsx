7c0f477c32744b0a555cd874271ac81325d9b39d
Abdar khan <129251163+Abdarkhan@users.noreply.github.com>
2023-10-07 01:20:13 +0500

    commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,73 @@
+import "./Footer.css";
+
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <h1 className="footer__title"> Abdar Khan</h1>
+
+        <ul className="footer__list">
+          <li>
+            <a href="#about" className="footer__link">
+              About
+            </a>
+          </li>
+
+          <li>
+            <a href="#services" className="footer__link">
+              Services
+            </a>
+          </li>
+
+          <li>
+            <a href="#testimonials" className="footer__link">
+              Testimonials
+            </a>
+          </li>
+        </ul>
+
+        <div className="footer__social">
+          <a
+            href="https://www.facebook.com/abdar.khan.9231/"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i class="bx bxl-facebook"></i>
+          </a>
+
+          <a
+            href="https://www.instagram.com/abdar.khan.9231/"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i class="bx bxl-instagram"></i>
+          </a>
+
+          <a
+            href="https://twitter.com/khanabdar038"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i class="bx bxl-twitter"></i>
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/abdar-khan-49b648234/"
+            className="footer__social-link"
+            target="_blank"
+          >
+            <i class="bx bxl-linkedin"></i>
+          </a>
+        </div>
+
+        <span className="footer__copy">
+          &#169; KK4-3KK. All rigths reserved
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/abdar.khan.9231/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-instagram"></i>
+      </a>
+
+      <a
+        href="https://www.facebook.com/abdar.khan.9231/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-facebook-f"></i>
+      </a>
+      
+      <a
+        href="https://twitter.com/khanabdar038"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-twitter-alt"></i>
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/abdar-khan-49b648234/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
